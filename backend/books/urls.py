452b3557538fbb8852b2abcfe6967a0e4b2e7328
SIRJAN MURMU <109529175+siru01@users.noreply.github.com>
2025-07-09 20:07:59 +0530

    Delete backend directory

--- backend/books/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import RegisterView, BookListCreateView
-
-urlpatterns = [
-    path('register/', RegisterView.as_view(), name='register'),
-    path('books/', BookListCreateView.as_view(), name='books'),
-]
